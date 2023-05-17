5a63a96255b834a6de05969f37cc38ec03112bed
Fergus Hayes <f.hayes.1@research.gla.ac.uk>
2023-05-17 16:00:04 +0100

    Now inputs data simpler

--- sample_model_nessai.py
+++ sample_model_nessai.py
@@ -20,7 +20,7 @@ import argparse, os, time
 
 np.random.seed(int(time.time()))
 
-def main(path, label, beta, structure, burn=300000, draws=100000, progressbar=False, interp=False, cores=None, Lfixed=False, default=True, random_seed=None, datapath=False):
+def main(path, structure, GW170817, GW190425, rates, beta=1, burn=2000, draws=1000, progressbar=False, interp=False, cores=None, Lfixed=False, default=False, random_seed=None, datapath='./inputdata/'):
 
     ######### Sample model #########
 
@@ -30,19 +30,31 @@ def main(path, label, beta, structure, burn=300000, draws=100000, progressbar=Fa
     cosi = []
     Nz = False
 
-    if not datapath:
-        datapath=path
+    label=''
 
-    for datafile in os.listdir(datapath):
-        if datafile[-8:]=='Flux.npy':
-            Y.append(np.load(datapath+datafile).squeeze())
-            F_sig.append(np.load(datapath+datafile[:-8]+'Flux_uncertainty.npy').squeeze())
-            d_, cosi_ = np.load(datapath+datafile[:-8]+'dcosi_samples.npy')
-            d.append(d_)
-            cosi.append(cosi_)
-        elif datafile=='rates.npy':
-            Nz = np.load(datapath+datafile)
-    
+    if rates:
+        label+='rates_'
+        Nz = np.load(datapath+'rates.npy')
+    if GW170817:
+        label+='GW170817_'
+        Y.append(np.load(datapath+'GW170817_Flux.npy').squeeze())
+        F_sig.append(np.load(datapath+'GW170817_Flux_uncertainty.npy').squeeze())
+        d_, cosi_ = np.load(datapath+'GW170817_dcosi_samples.npy')
+        d.append(d_)
+        cosi.append(cosi_)
+    if GW190425:
+        label+='GW190425_'
+        Y.append(np.load(datapath+'GW190425_Flux.npy').squeeze())
+        F_sig.append(np.load(datapath+'GW190425_Flux_uncertainty.npy').squeeze())
+        d_, cosi_ = np.load(datapath+'GW190425_dcosi_samples.npy')
+        d.append(d_)
+        cosi.append(cosi_)
+
+    if label=='':
+        raise ValueError('Please specify which data to analyze with at least one of the flags: --GW170817 --GW190425 --rates')
+
+    label+=structure
+
     if len(Y)==0:
         Y, F_sig, d, thetav = False, False, False, False
     else:
@@ -82,6 +94,11 @@ if __name__ == '__main__':
     parser.add_argument('--path', help="Path to run.")
     parser.add_argument('--label', help="Run label.")
     parser.add_argument('--datapath', help="Path to data.", default=False)
+
+    parser.add_argument('--GW170817', help="Add GW170817 data.", action='store_true', default=False)
+    parser.add_argument('--GW190425', help="Add GW190425 data.", action='store_true', default=False)
+    parser.add_argument('--rates', help="Add rates data.", action='store_true', default=False)
+
     parser.add_argument('--beta', help="Temperature of sampling.", default='1')
     parser.add_argument('--struct', help="Structure to analyse.", default=False)
     parser.add_argument('--burn', help="Number of samples to discard.", default=500000)
@@ -90,10 +107,10 @@ if __name__ == '__main__':
     parser.add_argument('--interp', help="Interpolation.", action='store_true', default=False)
     parser.add_argument('--Lfixed', help="Fix luminosity function.", action='store_true', default=False)
     parser.add_argument('--cores', help="Number of cores to use.", default=None)
-    parser.add_argument('--ndefault', help="See progressbar.", action='store_false', default=True)
+    parser.add_argument('--default', help="Default options (DEPRICATED).", action='store_true', default=False)
 
     parser.add_argument('--randomseed', help="Random seed.", default=None)
 
     opt = parser.parse_args()
 
-    main(opt.path, opt.label, float(opt.beta), opt.struct, int(opt.burn), int(opt.draws), bool(opt.progressbar), interp=opt.interp, cores=opt.cores, Lfixed=opt.Lfixed, default=opt.ndefault, random_seed=opt.randomseed, datapath=opt.datapath)
+    main(opt.path, opt.struct, opt.GW170817, opt.GW190425, opt.rates, beta=float(opt.beta), burn=int(opt.burn), draws=int(opt.draws), progressbar=bool(opt.progressbar), interp=opt.interp, cores=opt.cores, Lfixed=opt.Lfixed, default=opt.default, random_seed=opt.randomseed, datapath='./inputdata/')
